0aaa59649aa70bbbfbceb08659e1fbf34554d445
Precious <preciousemmanuel32@gmail.com>
2021-07-08 11:08:07 +0100

    modified page

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,7 +35,7 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
+    echo Yii::$app->user->isGuest ?"":  Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-left'],
         'items' => [
            
